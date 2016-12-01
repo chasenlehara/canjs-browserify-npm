dcc09ae4b6630fde5463026d49a9fe79aea82b47
Chasen Le Hara <chasen@chasenlehara.com>
2016-12-01 15:33:23 -0800

    Show using can-define/map/map

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-var util = require('can-util/can-util');
-console.log('can-util:', util);
+// main.js
+var DefineMap = require("can-define/map/map");
+var stache = require('can-stache/can-stache');
+
+var data = new DefineMap({message: "Hello World"});
+var template = stache('<h1>{{message}}</h1>');
+
+document.body.appendChild(template(data));
