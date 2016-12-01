1df3a0f8dccf4b819587e3de474d32362777a2a9
Chasen Le Hara <chasen@chasenlehara.com>
2016-12-01 15:41:28 -0800

    Use can-compilify

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var DefineMap = require("can-define/map/map");
 var stache = require('can-stache/can-stache');
 
 var data = new DefineMap({message: "Hello World"});
-var template = stache('<h1>{{message}}</h1>');
+var template = require("./main.stache");
 
 document.body.appendChild(template(data));
